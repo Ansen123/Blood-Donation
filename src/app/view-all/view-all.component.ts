629e7375dcb9880bdb2f400099b180eb2a21d267
Ansen123 <ansenfp@gmail.com>
2022-10-10 10:28:26 +0530

    view all

--- src/app/view-all/view-all.component.ts
+++ src/app/view-all/view-all.component.ts
@@ -9,11 +9,13 @@ export class ViewAllComponent implements OnInit {
 
   constructor() { }
 
-  donor1="Ansen"
-  donor2="Francis"
-  donor3="Pazhanilathu"
-  donor5="Nest"
-  donor4="Digital"
+  donorData=[{"name":"Ansen","Phone":9562661076},
+  {"name":"Ansen","Phone":9562667876},
+  {"name":"Firoz","Phone":9562674076},
+  {"name":"Joji","Phone":9562661078},
+  {"name":"Abin","Phone":9562661043},
+  {"name":"Tom","Phone":9562661074},
+]
 
   ngOnInit(): void {
   }
