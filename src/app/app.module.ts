685439770ee558865695dc91f81230b6d1026348
Ansen123 <ansenfp@gmail.com>
2022-10-13 15:39:54 +0530

    form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -39,7 +39,9 @@ const  appRoutes: Routes=[
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot(appRoutes),
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
